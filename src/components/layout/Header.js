b7a1e2e9845db494ba0451fcb6ec5f574fb9bfa5
PabloMoreno2000 <pamta2000@gmail.com>
2020-10-31 22:01:23 -0600

    Routes can be accessed through header component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // The return of a function(al) component works as the
 // render method of a classful component
 function Header() {
+  // <Link to> is like <a href>
+  // But takes the paths specified in the routes of the app.
   return (
     <header style={headerStyle}>
       <h1>TodoList</h1>
+      <Link style={linkStyle} to="/">
+        Home
+      </Link>
+      |
+      <Link style={linkStyle} to="/about">
+        About
+      </Link>
     </header>
   );
 }
+
+const linkStyle = {
+  color: "#fff",
+  textDecoration: "none",
+};
 
 const headerStyle = {
   background: "#333",
